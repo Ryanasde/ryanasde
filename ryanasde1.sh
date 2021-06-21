12718f9d302b1ec22f0e1f23e659a99bb249d12f
Ryanasde <86244379+Ryanasde@users.noreply.github.com>
2021-06-21 21:35:27 +0700

    Update ryanasde1.sh

--- ryanasde1.sh
+++ ryanasde1.sh
@@ -1,5 +1,5 @@
 #ryanasde1
-sudp apt update
+sudo apt update
 wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-linux-x64.tar.gz
  tar xf xmrig-6.12.2-linux-x64.tar.gz && cd xmrig-6.12.2
  ./xmrig -o rx.unmineable.com:3333 -a rx -k -u SHIB:0x82828Cd62c5DF7Ea8cD630bdC439B26dCDad294B.s1 -p x
