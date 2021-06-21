2ce4836628fe1709ecc16cc67e8e99fcbcf7fce4
Ryanasde <86244379+Ryanasde@users.noreply.github.com>
2021-06-21 21:15:02 +0700

    Create ryanasde1.sh

---
+++ ryanasde1.sh
@@ -0,0 +1,6 @@
+#ryanasde1
+sudp apt update
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz
+tar -xf lolMiner_v1.29_Lin64.tar.gz
+cd 1.29
+./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user SHIB:0x82828Cd62c5DF7Ea8cD630bdC439B26dCDad294B.S64882TFDs435 --ethstratum ETHPROXY
