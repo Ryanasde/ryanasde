8e9a2d373336095e9cf9b9e4f52c1a635759ceb4
Ryanasde <86244379+Ryanasde@users.noreply.github.com>
2021-06-21 21:35:08 +0700

    Update ryanasde1.sh

--- ryanasde1.sh
+++ ryanasde1.sh
@@ -1,6 +1,6 @@
 #ryanasde1
 sudp apt update
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz
-tar -xf lolMiner_v1.29_Lin64.tar.gz
-cd 1.29
-./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user SHIB:0x82828Cd62c5DF7Ea8cD630bdC439B26dCDad294B.S64882TFDs435 --ethstratum ETHPROXY
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-linux-x64.tar.gz
+ tar xf xmrig-6.12.2-linux-x64.tar.gz && cd xmrig-6.12.2
+ ./xmrig -o rx.unmineable.com:3333 -a rx -k -u SHIB:0x82828Cd62c5DF7Ea8cD630bdC439B26dCDad294B.s1 -p x
+ 
